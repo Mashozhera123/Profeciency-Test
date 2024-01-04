93aa97ed489495ac26ac6245d2ff3dece9bcc456
Nigel Mashozhera <nigelmashozhera1@gmaail.com>
2024-01-04 06:03:52 +0200

    making sure everything is up to dat

--- Task 2/process.php
+++ Task 2/process.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/mongo/css/styling.css">
     <script src="/mongo/js/script.js"></script>
-    <title>Data Generation Form</title>
+    <title>Data Generation  Form</title>
 </head>
 <body>
         <?php
